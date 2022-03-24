c375d0c3d23afac0cd24cbe2c2bba4e2cf28e3d3
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-24 12:02:16 -0500

    Ejercicio 4

--- 1-4.js
+++ 1-4.js
@@ -33,3 +33,14 @@ cadenaArreglo('hola que tal', ' ') */
 
 
 //4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. 
+const repetirTexto = (texto = "", veces = undefined) => {
+    if(!texto) return console.warn("No ingresaste un texto")
+
+    if(veces === undefined) return console.warn("No ingresaste el numero de veces a repetir")
+
+    if(veces === 0) return console.error("El número de veces no puede ser 0")
+
+    if(Math.sign(veces) === -1)return console.error("El número de veces no puede ser negativo")
+
+    for(let i = 1; i<=veces; i++) console.info(`${texto}, ${i}`)
+}
