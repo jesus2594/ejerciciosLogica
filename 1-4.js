d82f6bf69f57b2c90dac8945888510112ea9a9e4
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-24 11:16:05 -0500

    Ejercicio 3

--- 1-4.js
+++ 1-4.js
@@ -5,8 +5,8 @@ const contarCaracteres = (cadena = "") =>
     ? console.warn('No ingresaste ninguna cadena') 
     : console.info(`La cadena "${cadena}" tiene ${cadena.length} caracteres`)
 
-//contarCaracteres()
-//contarCaracteres("Hola Mundo")
+/* contarCaracteres()
+contarCaracteres("Hola Mundo") */
 
 //2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola". con validacion
 const recortarTexto = (cadena = "", longitud = undefined)=>
@@ -16,8 +16,20 @@ const recortarTexto = (cadena = "", longitud = undefined)=>
         ? console.warn("No ingresaste la longitud a recortar")
         : console.info(cadena.slice(0, longitud))
 
-recortarTexto()
+/* recortarTexto()
 recortarTexto("Hola Mundo", 4)
-recortarTexto("Hola Mundo")
+recortarTexto("Hola Mundo") */
+
+
 //3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+const cadenaArreglo = (cadena = "", separador = undefined)=>
+    (!cadena)
+    ? console.warn("No ingresaste una cadena de texto")
+    : (separador === undefined)
+        ? console.warn("No ingresaste el caracter separador")
+        : console.info(cadena.split(separador))
+/* cadenaArreglo()
+cadenaArreglo('hola que tal', ' ') */
+
+
 //4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. 
