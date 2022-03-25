540ff8ec18c4b4572ca5ab8de14db2b6b7640deb
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-25 14:23:25 -0500

    ejercicio 8

--- Ejercicios.js
+++ Ejercicios.js
@@ -91,8 +91,17 @@ const palindromo = (palabra = "")=> {
         ? console.info(`Si es palíndromo, Palabra original ${palabra}, Palabra al revés ${alReves}`)
         : console.info(`No es palíndromo, Palabra original ${palabra}, Palabra al revés ${alReves}`)
 }
-palindromo()
+/* palindromo()
 palindromo("hola Mundo")
 palindromo("salas")
-
-//8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+ */
+//8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("yxz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+const eliminarCaracteres = (texto = "", patron = "")=>
+    (!texto)
+      ? console.warn("No ingresaste un texto")
+      : (!patron)
+       ? console.warn("No ingresaste un patrón de caracteres")
+       : console.info(texto.replace(new RegExp(patron, "ig"),""));
+eliminarCaracteres();
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5")
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
