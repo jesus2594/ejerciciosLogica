0e4855f950c80ad0e718628322cea3b38b182e51
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-26 11:32:13 -0500

    Ejercicio 9

--- Ejercicios.js
+++ Ejercicios.js
@@ -102,6 +102,33 @@ const eliminarCaracteres = (texto = "", patron = "")=>
       : (!patron)
        ? console.warn("No ingresaste un patrón de caracteres")
        : console.info(texto.replace(new RegExp(patron, "ig"),""));
-eliminarCaracteres();
+/* eliminarCaracteres();
 eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5")
-eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz")
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") */
+
+//9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+const aleatorio = () => console.info(Math.round((Math.random() * 100) + 500));
+aleatorio();
+  //console.log(getRandomInt())
+//10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+/* const capicua = (numero = 0)=> {
+    if(!numero) return console.warn("No ingresaste ningun numero");
+
+    
+    let alReves = numero.reverse().join()
+
+    return (numero === alReves)
+        ? console.info(`Si es capicua, numero original ${numero}, numero al revés ${alReves}`)
+        : console.info(`No es capicua, numero original ${numero}, numero al revés ${alReves}`)
+} */
+//capicua(1991)
+//11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+/* function factorial(n){
+    let fact = 1
+    for(let i = 1; i <= n; i ++){
+        fact = fact * i
+    }
+    return fact
+}
+console.log(factorial(5)) */
