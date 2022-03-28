a939ea9f1f953a0e1a7acac4d330f52767946413
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-28 17:29:50 -0500

    Ejercicio 14

--- Ejercicios.js
+++ Ejercicios.js
@@ -189,9 +189,33 @@ const numeroParImpar = (numero = undefined) => {
         : console.info(`El número ${numero} es Impar`)
 }
 
-numeroParImpar()
+/* numeroParImpar()
 numeroParImpar("12")
 numeroParImpar(-12)
-numeroParImpar(19)
+numeroParImpar(19) */
 
 //14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+
+const convertirGrados = (grados = undefined, unidad = undefined) => {
+    if(grados === undefined) return console.warn("No ingresaste grados a convertir")
+
+    if(typeof grados !== "number") return console.error(`El valor "${grados}" ingresando, NO es un número`)
+
+    if(unidad === undefined) return console.warn("No ingresaste el tipo de grado a convertir")
+
+    if(typeof unidad!== "string") return console.error(`El valor "${unidad}" ingresando, NO es una cadena de texto`)
+
+    if(unidad.length !== 1 ||!/(C|F)/.test(unidad)) return console.warn("Valor de unidad no reconocido")
+
+    if(unidad === "C"){
+        return console.info(`${grados}°C = ${Math.round((grados * (9 / 5)) + 32)}°F`);
+    }else if(unidad === "F") {
+        return console.info(`${grados}°F =  ${Math.round(((grados - 32) * (5 / 9)))}°C`)
+    }
+}
+
+convertirGrados(100, "C")
+convertirGrados(32, "F")
+convertirGrados(100, "F")
+
+
