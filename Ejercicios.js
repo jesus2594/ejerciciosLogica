5d3c0613d09cefd93a568fa54806481aee4b4439
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-29 11:19:20 -0500

    Ejercicio 17

--- Ejercicios.js
+++ Ejercicios.js
@@ -265,12 +265,34 @@ const aplicarDescuento = (monto = undefined, descuento = 0) => {
 
 }
 
-aplicarDescuento()
+/* aplicarDescuento()
 aplicarDescuento("200")
 aplicarDescuento(0)
 aplicarDescuento(-1000)
 aplicarDescuento(1000, -20)
 aplicarDescuento(1000)
-aplicarDescuento(1000, 25)
+aplicarDescuento(1000, 25) */ 
 
 //17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020)
+
+
+const calcularAnios =(fecha = undefined) => {
+    if(fecha === undefined) return console.warn("No ingresaste la fecha")
+
+    if(!(fecha instanceof Date)) return console.error("El valor que ingresaste no es una fecha válida")
+
+    let hoyMenosFecha = new Date().getTime() - fecha.getTime(),
+    aniosEnMS = 1000 * 60 * 60 * 24 * 365
+    aniosHumanos = Math.floor(hoyMenosFecha / aniosEnMS)
+
+    return(Math.sign(aniosHumanos) === -1)
+        ? console.info(`Faltan ${Math.abs(aniosHumanos)} años para el ${fecha.getFullYear()}.`)
+        : (Math.sign(aniosHumanos) === 1)
+            ? console.info(`Han pasado ${aniosHumanos} años, desde ${fecha.getFullYear()}.`)
+            : console.info(`Estamos en el año actual ${fecha.getFullYear()}.`)
+}
+
+calcularAnios()
+calcularAnios({})
+calcularAnios(new Date())
+calcularAnios(new Date(1984, 4, 23))
