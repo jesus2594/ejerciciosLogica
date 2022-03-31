9fcfacf98d4ad4d5805df90e91a2af1523b6acd1
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-30 22:51:28 -0500

    Ejercicio 20

--- Ejercicios.js
+++ Ejercicios.js
@@ -342,9 +342,26 @@ const validarNombre = (nombre = "") => {
         : console.warn(`"${nombre}", NO es un nombre válido`);
 }
 
-validarNombre()
+/* validarNombre()
 validarNombre(3)
 validarNombre("Juan Lopez")
-validarNombre("Juan Lopez, 19")
+validarNombre("Juan Lopez, 19") */
 
 //20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+
+const validarEmail = (email = "") => {
+    if(!email) return console.warn("No ingresaste un email")
+
+    if(typeof email !== "string") return console.error(`El valor "${email}" ingresado, NO es una cadena de texto`);
+
+    let expReg = /[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email);
+
+    return (expReg)
+        ? console.info(`"${email}", es un email válido`)
+        : console.warn(`"${email}", NO es un email válido`);
+}
+
+validarEmail()
+validarEmail(34)
+validarEmail("juan,lopez@gmail")
+validarEmail("juanlopez@gmail.com")
