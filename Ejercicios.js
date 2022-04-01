fd7acf922cc15364b5513a6677089c26e131f319
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-31 22:18:32 -0500

    Ejercicio 23

--- Ejercicios.js
+++ Ejercicios.js
@@ -425,13 +425,35 @@ const arrayMinMax = (arr = undefined) => {
     return console.info(`Arreglo original: ${arr}\nValor mayor: ${Math.max(...arr)},\nValor menor: ${Math.min(...arr)}`);
 }
 
-arrayMinMax()
+/* arrayMinMax()
 arrayMinMax(false)
 arrayMinMax([])
 arrayMinMax([2, 3, true])
-arrayMinMax([1, 4, 5, 99, -60])
+arrayMinMax([1, 4, 5, 99, -60]) */
 
 
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
 
+const separarParesImpares = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío")
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`)
+    }
+
+    return console.info({
+        pares: arr.filter(num => num % 2 === 0),
+        impares: arr.filter(num => num % 2 === 1)
+    })
+}
+
+separarParesImpares()
+separarParesImpares("hola")
+separarParesImpares([])
+separarParesImpares([2, 4, "j"])
+separarParesImpares([1,2,3,4,5,6,7,8,9,0])
