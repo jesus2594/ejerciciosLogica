c3980f80e8f6bb525416dfcee15879e0a3e0e9f8
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-01 08:58:42 -0500

    Ejercicio 25

--- Ejercicios.js
+++ Ejercicios.js
@@ -477,11 +477,30 @@ const ordenarArreglo = (arr = undefined) => {
         desc: arr.map(el => el).sort().reverse()
     })
 }
-ordenarArreglo()
+/* ordenarArreglo()
 ordenarArreglo("hola")
 ordenarArreglo([])
 ordenarArreglo([3, {}])
-ordenarArreglo([7, 5,7,8,6])
-//25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
+ordenarArreglo([7, 5,7,8,6]) */
 
+//25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
+const quitarDuplicados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío")
+
+    if(arr.length === 1) return console.warn("El arreglo debe tener al menos 2 elementos")
+
+    return console.info({
+        original: arr,
+        sinDuplicados: arr.filter((value, index, self) => self.indexOf(value) === index)
+    }) 
+}
+quitarDuplicados()
+quitarDuplicados({})
+quitarDuplicados([])
+quitarDuplicados([2])
+quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true])
 //26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
