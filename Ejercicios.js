5875d56a3ef58091ce2875cd5b636f508c7371f3
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-31 22:07:20 -0500

    Ejercicio 22

--- Ejercicios.js
+++ Ejercicios.js
@@ -400,14 +400,37 @@ const devolverCuadrados = (arr = undefined) => {
     return console.info(`Arreglo original: ${arr},\nArreglo elevado al cuadrado: ${newArray}`);
 }
 
-devolverCuadrados()
+/* devolverCuadrados()
 devolverCuadrados(true)
 devolverCuadrados({})
 devolverCuadrados([])
 devolverCuadrados([1, "3", 4, {}])
 devolverCuadrados([1, 4, {}])
-devolverCuadrados([1, 4, 5])
+devolverCuadrados([1, 4, 5]) */
+
 //22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+
+const arrayMinMax = (arr = undefined) => {
+
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío")
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`)
+    }
+
+    return console.info(`Arreglo original: ${arr}\nValor mayor: ${Math.max(...arr)},\nValor menor: ${Math.min(...arr)}`);
+}
+
+arrayMinMax()
+arrayMinMax(false)
+arrayMinMax([])
+arrayMinMax([2, 3, true])
+arrayMinMax([1, 4, 5, 99, -60])
+
 
 //23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
