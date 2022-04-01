d27fa7b0d18d80a51c9c2b483b6cac1e5a287142
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-31 21:53:59 -0500

    Ejercicio 21

--- Ejercicios.js
+++ Ejercicios.js
@@ -361,7 +361,54 @@ const validarEmail = (email = "") => {
         : console.warn(`"${email}", NO es un email válido`);
 }
 
-validarEmail()
+/* validarEmail()
 validarEmail(34)
 validarEmail("juan,lopez@gmail")
-validarEmail("juanlopez@gmail.com")
+validarEmail("juanlopez@gmail.com") */
+
+/* Fusión 19 y 20 */
+const validarPatron = (cadena = "", patron = "") => {
+    if(!cadena) return console.warn("No ingresaste un email")
+
+    if(typeof cadena !== "string") return console.error(`El valor "${cadena}" ingresado, NO es una cadena de texto`);
+
+    if(!patron === undefined) return console.warn("No ingresaste un patron a evaluar")
+
+    if(!(patron instanceof RegExp)) return console.error(`El valor "${patron}" ingresado, NO es una expresion regular`);
+
+    let expReg = patron.test(cadena);
+
+    return (expReg)
+        ? console.info(`"${cadena}", cumple con el patrón ingresado`)
+        : console.warn(`"${cadena}", NO cumple con el patrón ingresado`);
+}
+
+//21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+const devolverCuadrados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío")
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`)
+    }
+
+    const newArray = arr.map(el => el * el)
+
+    return console.info(`Arreglo original: ${arr},\nArreglo elevado al cuadrado: ${newArray}`);
+}
+
+devolverCuadrados()
+devolverCuadrados(true)
+devolverCuadrados({})
+devolverCuadrados([])
+devolverCuadrados([1, "3", 4, {}])
+devolverCuadrados([1, 4, {}])
+devolverCuadrados([1, 4, 5])
+//22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+
+//23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+
+
