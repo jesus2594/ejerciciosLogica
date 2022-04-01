ee69702f2d43bd76fd3e1a95bc834c4ad48a33fa
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-01 09:18:33 -0500

    Ejercicio 25

--- Ejercicios.js
+++ Ejercicios.js
@@ -493,14 +493,51 @@ const quitarDuplicados = (arr = undefined) => {
 
     if(arr.length === 1) return console.warn("El arreglo debe tener al menos 2 elementos")
 
-    return console.info({
+    /* return console.info({
         original: arr,
         sinDuplicados: arr.filter((value, index, self) => self.indexOf(value) === index)
-    }) 
+    })  */
+    //Con SET
+    return console.info({
+        original: arr,
+        sinDuplicados: [... new Set(arr)]
+    })
 }
-quitarDuplicados()
+
+/* quitarDuplicados()
 quitarDuplicados({})
 quitarDuplicados([])
 quitarDuplicados([2])
-quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true])
+quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true]) */
+
+
 //26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+const promedio = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío")
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`)
+    }
+
+    return console.info(
+        arr.reduce((total, num, index, arr) => {
+            total += num
+            if(index === arr.length -1){
+                return `El promedio de ${arr.join(" + ")} es ${total / arr.length}`
+            }else {
+                return total
+            }
+           
+        })
+    )
+}
+
+promedio()
+promedio({})
+promedio([])
+promedio([2, true])
+promedio([9,8,7,6,5,4,3,2,1,0])
