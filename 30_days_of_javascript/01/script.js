656c979087add3f44d4cc90f3a27ebc7deb2ca9e
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-21 17:08:00 -0500

    Ejercicios

---
+++ 30_days_of_javascript/01/script.js
@@ -0,0 +1,52 @@
+/* console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined */
+
+/* Write a single line comment which says, comments can make code readable
+
+Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+Declare four variables without assigning values
+
+Declare four variables with assigned values
+
+Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+Declare variables to store your first name, last name, marital status, country and age in a single line
+
+Declare two variables myAge and yourAge and assign them initial values and log to the browser console. */
+
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/* comments can make code readable,
+ easy to reuse and informative */
+
+let a = "prueba"
+let b = true
+let c
+let d = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+
+const firstName = "jesus"
+const lastName = "marmol"
+let maritalStatus = "single"
+const country = "colombia"
+
+let firstName2 = "jesus",  lastName2 = "marmol",  maritalStatus2 = "single",  country2 = "colombia"
+let myAge = 27
+let yourAge = 25
+
+console.log("I am " + myAge + " years old.")
+console.log("You are " + yourAge + " years old.")
