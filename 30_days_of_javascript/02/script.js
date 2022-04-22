3090135a017f2fc9de5ef859e711f744ea85966f
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-22 17:40:34 -0500

    exercises Level 3

--- 30_days_of_javascript/02/script.js
+++ 30_days_of_javascript/02/script.js
@@ -152,3 +152,28 @@ const strSlice = 'You cannot end a sentence with because because because is a co
 //console.log(strSlice.lastIndexOf('because'))
 console.log(strSlice.substr(31, 23))
 
+/* Level 3 */
+
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+const str3 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let pattern2 = /love/gi
+let numberOfWord = str3.match(pattern2)
+console.log("word love appears " +numberOfWord.length+ " times")
+
+
+//Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+const str4 = 'You cannot end a sentence with because because because is a conjunction'
+let pattern3 = /because/gi
+let numberOfWord2 = str4.match(pattern3)
+console.log("because appears " + numberOfWord2.length + " times")
+
+//Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+const sentence5 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+sentence5.replace()
+
+
+ //Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+ const text = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+ let arr = text.match(/\d+/g)
+ console.log(arr)
+
