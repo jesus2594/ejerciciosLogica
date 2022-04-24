ff7b3844156e876068ce4970b073265bfe2f4441
Jesus Marmol <jesusdavid890@hotmail.com>
2022-04-24 12:36:31 -0500

    Exercises level 1

---
+++ 30_days_of_javascript/03/script.js
@@ -0,0 +1,124 @@
+//Exercises: Level 1
+
+//Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+/*let firstName = 'Jesus'
+let lastName = 'Marmol'
+let country = 'Colombia'
+let city = 'Barranquila'
+let age = 27
+let isMarried = false
+let year = 2022
+ console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year) */
+
+//Check if type of '10' is equal to 10
+//console.log( '10' === 10)
+
+//Check if parseInt('9.8') is equal to 10
+//console.log(parseInt('9.8') == 10)
+
+/* Boolean value is either true or false.
+Write three JavaScript statement which provide truthy value.
+console.log(10 > 5)
+console.log(0 === 0)
+console.log(true)
+Write three JavaScript statement which provide falsy value.
+console.log(7 < 1)
+console.log(1 !== 1)
+console.log(false) */
+
+//Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+4 > 3  //true
+4 >= 3 //true
+4 < 3  //false
+4 <= 3 //false
+4 == 4 //true
+4 === 4 //true
+4 != 4 //false
+4 !== 4 //false
+4 != '4' //false
+4 == '4' //true
+4 === '4' //false
+/* console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4') */
+
+//Find the length of python and jargon and make a falsy comparison statement.
+//console.log('python'.length !== 'jargon'.length)
+
+//Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+
+4 > 3 && 10 < 12     //true
+4 > 3 && 10 > 12     //false
+4 > 3 || 10 < 12     //true
+4 > 3 || 10 > 12     //true
+!(4 > 3)             //false
+!(4 < 3)             //true
+!(false)             //true
+!(4 > 3 && 10 < 12)  //false
+!(4 > 3 && 10 > 12)  //true
+!(4 === '4')        //true
+//There is no 'on' in both dragon and python //true
+
+/* console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(!(4 > 3))
+console.log(!(4 < 3) )
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+console.log('dragon'.includes('on') && 'python'.includes('on'))
+ */
+
+/* 
+Use the Date object to do the following activities
+What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now.
+ */
+/* const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getTime())
+ */
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
